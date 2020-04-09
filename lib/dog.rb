5854056da3fa7d4ba4ad587a31611bba5170bb20
Yakura Marzett <marzettyakura@yahoo.com>
2020-04-09 20:55:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+class Dog
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end
+end
+fido = Dog.new("Fido") 
+
+
+
+class Dog
+  def initialize(name, breed = "Mut")
+    @name = name
+    @breed = breed
+  end
+end
+fido = Dog.new("Fido")
